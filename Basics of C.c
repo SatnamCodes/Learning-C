6ed473a098ae0a8b559f26c1f95eec20f86e47c2
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-23 20:20:43 +0530

    Intial commit

--- Basics of C.c
+++ Basics of C.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
-int main() {
-printf("%d", sizeof(int));
-return 0;
+int main(){
+    printf("Hello, Anonymous!\n");
+    printf("Make a dent in the universe!");
+    //this is the way to print something in C
+    //But as we know , it gets printed into the same line , if we want to print something in the next line we can use \n
+
+    printf("Hello anon");
+    printf("Make a dent in the universe!\n");
 }
