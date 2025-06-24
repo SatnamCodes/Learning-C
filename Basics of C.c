3a8772b078ef7fd107c00da38b5cb56b11471646
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-24 18:57:15 +0530

    renamed basics of c.c to learning-c.c

--- Basics of C.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    char star = "*";
-    printf("This is what a star looks like %c/n", star)
-} 
---
+++ learning-c.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    char star = "*";
+    printf("This is what a star looks like %c/n", star);
+    return 0;
+}
