e2702cfa1a8828a91f2b8538c4e0acaaf0a424d4
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-24 19:01:42 +0530

    Format specifiers

--- learning-c.c
+++ learning-c.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    char star = "*";
-    printf("This is what a star looks like %c/n", star);
+    char star = '*';
+    printf("This is what a star looks like %c\n", star);
     return 0;
 }
+
+// Lesson : Learnt about format specifiers with a single character *
