6ccd6f640adbf238ef0346764bc629c0ec131761
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-24 18:49:26 +0530

    refactored everything

--- Basics of C.c
+++ Basics of C.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello, Anonymous!\n");
-    printf("Make a dent in the universe!");
-    //this is the way to print something in C
-    //But as we know , it gets printed into the same line , if we want to print something in the next line we can use \n
-
-    printf("Hello anon");
-    printf("Make a dent in the universe!\n");
-}
+    char star = "*";
+    printf("This is what a star looks like %c/n", star)
+} 
