a96860e2f7c006354866073e9c5b127e7f44a36d
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-26 10:05:51 +0530

    Format Specifiers

--- learning-c.c
+++ learning-c.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
-
+//Basic structure of a c program.
 int main(){
+    printf("Hello Anon.");
+    //A basic print function.
     char star = '*';
-    printf("This is what a star looks like %c\n", star);
+    printf("\nThis is what a star looks like %c\n ", star );
+    //Format specifiers with a single character.
+    printf("\n**********************");
+    printf("\n*I love C programming*");
+    printf("\n**********************");
     return 0;
 }
 
-// Lesson : Learnt about format specifiers with a single character *
