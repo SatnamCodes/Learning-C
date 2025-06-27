1a142e71833c5a8fa75049e3cab019fd74eb9bd0
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-27 23:45:22 +0530

    feat: add functions to demonstrate variable value changes and multiple variable usage

--- 01_Basics/Variables.c
+++ 01_Basics/Variables.c
@@ -1,5 +1,10 @@
 //A variable is a named storage location in the memory that can hold a specific type of data and a value associted with it.
 #include <stdio.h>
+
+void value_change();
+void multiple_variables();
+
+
 int main (){
     char name[] = "John F Kennedy"; //Strings are defined using the char data type in C.
     int age = 46; //int is the integer data type used to store whole numbers.
@@ -9,5 +14,22 @@ int main (){
     printf("Age: %d\n",age); //%d is the format specifier for integers.
     printf("Height: %.2f meters\n",height); //%.2f is used to format the float to 2 decimal places.
     printf("Initial: %c\n",initial); //%c is the format specifier for characters.
-    return 0;
+    value_change(); //Function call to demonstrate changing variable values.
+    multiple_variables(); //Function call to demonstrate multiple variable declaration and usage.
+}
+
+void value_change() {
+    int x = 10;
+    printf("Value of x before change: %d\n", x);
+    x = 20; //Changing the value of x
+    printf("Value of x after change: %d\n", x);
+}
+
+
+void multiple_variables() {
+    int x = 5, y = 6, z = 50;
+    printf("%d", x + y + z);
+    
+
+
 }
