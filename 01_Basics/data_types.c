c0d39403fe11ff509baa508cc4aee82128dc6480
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-06-28 11:41:30 +0530

    feat: add initial implementation of data types in C with examples and format specifiers

---
+++ 01_Basics/data_types.c
@@ -0,0 +1,36 @@
+// This file contains Data Types in C with examples and full codes written out.
+//All the variables must have a specifierd data type and we have to use a format specifier in order to tell the compiler about the data type we want to process and receive an output.
+//Here is a code for different data types and format specifiers in C.
+#include <stdio.h> 
+//Basic data types in C
+
+void modified_data_types();
+void derived_data_types();
+void pointers_in_c();
+void functions_in_c();
+void structures_in_c();
+void unions();
+void enumerations_in_c();
+void void_functions();
+void void_pointers();
+
+int main(){
+    //Basic data types.
+    char character = "My name is Satnam."; //Characters.
+    int age = 10; //Integers without decimal points.
+    float value_of_pi = 3.14; //Fractional values with 6-7 digits.)
+    double lol_large_number = 123456789.987654321; //Fractional values with upto 15 digits.
+    short small_number = 5;
+    printf("Character: %s\n", character); //%s is a format specifier for strings.
+    printf("I am %d years old.\n", age); //%d is a format specifier for integers.
+    printf("The value of pi is %f\n", value_of_pi);//%f is the format specifier for floats.
+    printf("Here is a large number %lf\n", lol_large_number); //%lf is the format specifier for doubles.
+    printf("Here is a small number %hd\n", small_number); //%hd is
+}   
+
+
+void modified_data_types(){
+    // Here are the modified data types in C.
+
+}
+
