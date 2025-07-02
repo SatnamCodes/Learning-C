7212bf02f6e5139fcd5335d8f9c5c140d893781e
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-07-02 15:16:21 +0530

    feat: Update main function to correct character string and add sum calculation for user input

---
+++ 01_Basics/sum_with_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main {}(
+    char character[] = "My name is Satnam."; //Characters.
+    int num1 ,num2,num3; //Integers without decimal points.
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &num1, &num2, &num3); //Taking input from the user.
+    printf("You entered: %d, %d, %d\n", num1, num2, num3); //Printing the integers entered by the user.
+)
