68c7f516e15fb949691f6d88cbc14c5b9990155d
Satnam Singh <139437542+SatnamCodes@users.noreply.github.com>
2025-07-06 16:16:00 +0530

    refactor: Improve code structure and formatting in data_types.c; add user input and sum calculation

--- 01_Basics/data_types.c
+++ 01_Basics/data_types.c
@@ -1,64 +1,93 @@
-// This file contains Data Types in C with examples and full codes written out.
-//All the variables must have a specified data type and we have to use a format specifier in order to tell the compiler about the data type we want to process and receive an output.
-//Here is a code for different data types and format specifiers in C.
-#include <stdio.h> 
-//Basic data types in C
+/*
+ * This file contains Data Types in C with examples and full codes written out.
+ * All variables must have a specified data type and we have to use a format 
+ * specifier in order to tell the compiler about the data type we want to 
+ * process and receive an output.
+ */
 
-// Data Type Examples
+#include <stdio.h>
+
+// Function declarations.
 void modified_data_types();
 void derived_data_types();
 void pointers_in_c();
-
-// Code Constructs
 void functions_in_c();
 void void_functions();
 void void_pointers();
-
-// User-defined Types
 void structures_in_c();
 void unions_in_c();
 void enumerations_in_c();
 
-
-int main(){
-    //Basic data types and the basic input.
-    char character[] = "My name is joeyyy."; //Characters.
-    int age = 10; //Integers without decimal points.
-    float value_of_pi = 3.14; //Fractional values with 6-7 digits.)
-    double lol_large_number = 123456789.987654321; //Fractional values with upto 15 digits.
-    short small_number = 5;
-    int num1 ,num2,num3; //Integers without decimal points.
-    printf("Character: %s\n", character); //%s is a format specifier for strings.
-    printf("I am %d years old.\n", age); //%d is a format specifier for integers.
-    printf("The value of pi is %f\n", value_of_pi);//%f is the format specifier for floats.
-    printf("Here is a large number %lf\n", lol_large_number); //%lf is the format specifier for doubles.
-    printf("Here is a small number %hd\n", small_number); 
-    //%hd is the format specifier for short integers.
+int main() {
+    // Basic data types.
+    char character[] = "My name is joeyyy.";           // String (array of characters)
+    int age = 10;                                      // Integer without decimal points
+    float value_of_pi = 3.14f;                         // Float with 6-7 digits precision
+    double large_number = 123456789.987654321;         // Double with up to 15 digits precision
+    short small_number = 5;                            // Short integer
+    int num1, num2, num3;                              // User input integers
     
+    // Display basic data types .
+    printf("=== Basic Data Types Demo ===\n");
+    printf("Character: %s\n", character);              // %s for strings
+    printf("I am %d years old.\n", age);               // %d for integers
+    printf("The value of pi is %.2f\n", value_of_pi);  // %f for floats
+    printf("Here is a large number %.9lf\n", large_number); // %lf for doubles
+    printf("Here is a small number %hd\n", small_number);   // %hd for short integers
     
-    //Taking input from the user.
+    // Taking input from the user
+    printf("\n=== User Input Demo ===\n");
     printf("Enter three integers: ");
-    scanf("%d %d %d", &num1, &num2, &num3); //Taking input from the user.
-    printf("You entered: %d, %d, %d\n", num1, num2, num3); //Printing the integers entered by the user.
-    printf("The sum of the three integers is: %d\n", num1 + num2 + num3); //Calculating and printing the sum of the three integers.
+    scanf("%d %d %d", &num1, &num2, &num3);
     
-}   
-
-
-void modified_data_types(){
-    // Here are the modified data types in C.
-
-
+    // Display user input and calculations
+    printf("You entered: %d, %d, %d\n", num1, num2, num3);
+    printf("The sum of the three integers is: %d\n", num1 + num2 + num3);
+    
+    return 0;
 }
 
-void derived_data_types(){
-
+// Modified data types (signed, unsigned, long, etc.)
+void modified_data_types() {
+    // Implementation for modified data types goes here
 }
 
-void pointers_in_c(){
-
+// Derived data types (arrays, pointers, functions)
+void derived_data_types() {
+    // Implementation for derived data types goes here
 }
 
-void functions_in_c(){
+// Pointer concepts and examples
+void pointers_in_c() {
+    // Implementation for pointers goes here
+}
 
+// Function concepts and examples
+void functions_in_c() {
+    // Implementation for functions goes here
+}
+
+// Void functions demonstration
+void void_functions() {
+    // Implementation for void functions goes here
+}
+
+// Void pointers demonstration
+void void_pointers() {
+    // Implementation for void pointers goes here
+}
+
+// Structure concepts and examples
+void structures_in_c() {
+    // Implementation for structures goes here
+}
+
+// Union concepts and examples
+void unions_in_c() {
+    // Implementation for unions goes here
+}
+
+// Enumeration concepts and examples
+void enumerations_in_c() {
+    // Implementation for enumerations goes here
 }
