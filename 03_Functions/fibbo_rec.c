9ce32671fc232167956bb70af2d10561c6c1f1ab
Satnam <139437542+SatnamCodes@users.noreply.github.com>
2025-09-24 23:45:10 +0530

    feat: Implement Fibonacci sequence calculation in fibbo_rec.c

---
+++ 03_Functions/fibbo_rec.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+int fib(int n){
+    if (n==0){
+        return 0;
+    }
+    else if (n==1){
+        return 1;
+    }
+    else 
+    return fib(n-1)+fib(n-2);
+}
+int main (){
+    int num,i;
+    printf("Enter the number :");
+    scanf("%d",&num);
+    if (num<0){
+        printf("Kindly enter a positive number.\n");
+
+    }
+    else {
+        for (i=0;i<num;i++){
+        printf("%d ", fib(i));
+    }
+    printf("\n");
+}
+
+
+}
+
